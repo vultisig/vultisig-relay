516f166e2efc4e2ecc1e12b8576283a2fe3c162d
webpiratt <213060745+webpiratt@users.noreply.github.com>
2025-12-23 22:22:41 +0300

    deploy

--- cmd/router/main.go
+++ cmd/router/main.go
@@ -13,14 +13,19 @@ func main() {
 	flag.StringVar(&cfgFile, "config", "config.json", "config file")
 	flag.Parse()
 
-	cfg, err := config.LoadConfig(cfgFile)
+	cfg := config.LoadConfig(cfgFile)
+
+	var store *storage.RedisStorage
+	var err error
+	if cfg.RedisURI != "" {
+		store, err = storage.NewRedisStorageFromURI(cfg.RedisURI)
+	} else {
+		store, err = storage.NewRedisStorage(cfg.RedisServer)
+	}
 	if err != nil {
 		panic(err)
 	}
-	store, err := storage.NewRedisStorage(cfg.RedisServer)
-	if err != nil {
-		panic(err)
-	}
+
 	s := server.NewServer(cfg.Port, store)
 	if err := s.StartServer(); err != nil {
 		panic(err)
